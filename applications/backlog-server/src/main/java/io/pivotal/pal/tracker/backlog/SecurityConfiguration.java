bae588f8bc6e5bc290d7fbad9bfc969a9165377a
ashish8024571 <ashish.jain5@rbs.co.uk>
2019-09-03 16:26:22 +0530

    SSO implementation

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/SecurityConfiguration.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/SecurityConfiguration.java
@@ -1,5 +1,5 @@
 package io.pivotal.pal.tracker.backlog;
-
+/*
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,3 +19,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
 }
+
+ */
